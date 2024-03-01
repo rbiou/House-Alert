e6704be151a7715ae42bce723cb3b9945d850021
rbiou <remi.biou@canal-plus.com>
2024-03-02 00:03:32 +0100

    fix: import for db connexion

--- db_connexion.py
+++ db_connexion.py
@@ -1,12 +1,12 @@
 import os
 import psycopg2
 
-from local import get_DB_URI
 from utils import log
 
 def get_connexion():
     log("Start getting db connexion...")
     if os.path.isfile("local.py"):
+        from local import get_DB_URI
         DB_URI = get_DB_URI()
     else:
         DB_URI = os.environ['DB_URI']
